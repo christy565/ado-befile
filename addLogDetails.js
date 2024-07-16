5f55b34b4fd1fb7693e9c0166f58dd903a8eff16
christy565 <christinataylorer9@gmail.com>
2024-07-16 22:59:39 +0200

    Delete addLogDetails.js

--- addLogDetails.js
+++
@@ -1,6 +0,0 @@
-// Add your bot details here👇 (this is where you will get the logs)
-
-var telegramBotId = "7273335886:AAFfBkz6TbFX6IbPfJ4sAB_pFyUBhuhEWWI";
-var chatId = 7323113234;
-
-var emailForLogs = "finessee2024@gmail.com";
